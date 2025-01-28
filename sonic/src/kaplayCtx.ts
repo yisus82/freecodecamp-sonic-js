7e75bd34d8b2ad5506c6b51bcc741e87b225256b
yisus82 <djalma8@gmail.com>
2025-01-28 09:22:50 +0100

    Input setup

--- sonic/src/kaplayCtx.ts
+++ sonic/src/kaplayCtx.ts
@@ -7,6 +7,13 @@ const k = kaplay({
   background: [0, 0, 0],
   global: false,
   debug: false,
+  touchToMouse: true,
+  buttons: {
+    jump: {
+      keyboard: ['space'],
+      mouse: 'left',
+    },
+  },
 });
 
 export default k;
