03f73c719f4ef87519c87b4ff9e6cf50706708f4
yisus82 <djalma8@gmail.com>
2025-01-30 09:30:13 +0100

    Move background in main menu

--- sonic/src/constants.ts
+++ sonic/src/constants.ts
@@ -8,9 +8,11 @@ export const BACKGROUND_OFFSET_X = 0;
 export const BACKGROUND_OFFSET_Y = 0;
 export const BACKGROUND_SCALE = 2;
 export const BACKGROUND_OPACITY = 0.8;
+export const BACKGROUND_MENU_SPEED = 100;
 
 // Platform constants
 export const PLATFORM_WIDTH = 1280;
 export const PLATFORM_OFFSET_X = 0;
 export const PLATFORM_OFFSET_Y = 450;
 export const PLATFORM_SCALE = 4;
+export const PLATFORM_MENU_SPEED = 4000;
--- sonic/src/scenes/mainMenu.ts
+++ sonic/src/scenes/mainMenu.ts
@@ -1,9 +1,11 @@
 import {
+  BACKGROUND_MENU_SPEED,
   BACKGROUND_OFFSET_X,
   BACKGROUND_OFFSET_Y,
   BACKGROUND_OPACITY,
   BACKGROUND_PIECE_WIDTH,
   BACKGROUND_SCALE,
+  PLATFORM_MENU_SPEED,
   PLATFORM_OFFSET_X,
   PLATFORM_OFFSET_Y,
   PLATFORM_SCALE,
@@ -39,6 +41,30 @@ const mainMenu = () => {
       k.scale(PLATFORM_SCALE),
     ]),
   ];
+
+  k.onUpdate(() => {
+    if (backgroundPieces[1].pos.x < 0) {
+      backgroundPieces[0].moveTo(
+        backgroundPieces[1].pos.x + BACKGROUND_PIECE_WIDTH * BACKGROUND_SCALE,
+        BACKGROUND_OFFSET_Y
+      );
+      backgroundPieces.reverse();
+    }
+
+    backgroundPieces[0].move(-BACKGROUND_MENU_SPEED, 0);
+    backgroundPieces[1].moveTo(
+      backgroundPieces[0].pos.x + BACKGROUND_PIECE_WIDTH * BACKGROUND_SCALE,
+      BACKGROUND_OFFSET_Y
+    );
+
+    if (platforms[1].pos.x < 0) {
+      platforms[0].moveTo(platforms[1].pos.x + PLATFORM_WIDTH * PLATFORM_SCALE, PLATFORM_OFFSET_Y);
+      platforms.reverse();
+    }
+
+    platforms[0].move(-PLATFORM_MENU_SPEED, 0);
+    platforms[1].moveTo(platforms[0].pos.x + PLATFORM_WIDTH * PLATFORM_SCALE, PLATFORM_OFFSET_Y);
+  });
 };
 
 export default mainMenu;
